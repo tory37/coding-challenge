7a199b36f0838e369406036b4f264d0dfafc635c
tory37@gmail.com <tory37@gmail.com>
2017-07-05 21:19:00 -0500

    serving app from node

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+    path = require('path'),
+    fs = require('fs'),
     app = express(),
     port = process.env.PORT || 3000,
 
@@ -15,9 +17,39 @@ mongoose.connect('mongodb://localhost/Waitrchallengedb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// app.use(function(req, res, next) {
+//     var accept = req.accepts('html', 'json', 'xml');
+//     if (accept !== 'html') {
+//         return next();
+//     }
+
+//     var ext = path.extname(req.path);
+//     if (ext !== '') {
+//         return next();
+//     }
+
+//     fs.createReadStream(staticRoot + 'index.html').pipe(res);
+// })
+
+
+
 var routes = require('./api/routes/driverRoutes');
 routes(app);
 
-app.listen(port);
+app.listen(port, function () {
+    console.log('App running on port' + port);
+});
 
 console.log('todo list RESTful API server started on: ' + port);
